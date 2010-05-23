111dd70193beb0c2e8c580f62ca2c713acb74104
Reid Burke <me@reidburke.com>
2010-05-23 10:18:27 -0400

    Autocomplete fun time!

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-// hello world
+YUI().use("node", "json", "io", "yui2-autocomplete", function (Y) {    
+console.log("HELLO");
+
+    Y.io("api/friends", {
+        on : {
+            success : function (response) {
+                var friends = Y.JSON.parse(response.responseText);
+
+                YAHOO = Y.YUI2;
+
+                // Use a LocalDataSource
+                var oDS = new YAHOO.util.LocalDataSource(friends);
+                // Optional to define fields for single-dimensional array
+                oDS.responseSchema = {fields : ["name"]};
+             
+                // Instantiate the AutoComplete
+                var oAC = new YAHOO.widget.AutoComplete("name", "name-dropdown", oDS);
+                oAC.prehighlightClassName = "yui-ac-prehighlight";
+                oAC.useShadow = true;
+            },
+            failure : function (resp) {
+                console.log("FAIL", resp);
+            }
+        }
+    });
+
+});
